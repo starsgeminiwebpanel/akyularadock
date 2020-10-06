75a37d675900b7a4e680a500869b8347aaaa00f4
artigeministars <@1Github@>
2020-10-07 00:01:16 +0300

    added vue material icons url

--- Modules/Frontsection/Resources/views/index.blade.php
+++ Modules/Frontsection/Resources/views/index.blade.php
@@ -2,12 +2,12 @@
 <div id="frontsectionapp">
     <app-f-section> </app-f-section>
 </div>
+{{--
 @section('content')
     <h1>Hello World</h1>
-{{-- comment blade template --}}
 <p> This view is loaded from module: {!! config('frontsection.name') !!} </p>
 @endsection
-
+--}}
 
 <script src="{{ mix('/js/frontsection.js') }}"></script>
 
